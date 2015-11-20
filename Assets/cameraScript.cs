3664a265a528a99c994b234b8109bc3804158dd9
Esdras Martins <esdras.firminom@gmail.com>
2015-11-20 02:34:25 -0200

    Script de camera

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+
+	public Transform target;
+	public float distance;
+
+	// Update is called once per frame
+	void Update () {
+		float positionZ = target.position.z - distance;
+		float positionY = target.position.y;
+		float positionX = target.position.x;
+
+
+		transform.position = new Vector3 (positionX, transform.position.y, positionZ);
+	}
+}
